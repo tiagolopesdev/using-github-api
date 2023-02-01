ffaa790ef72c9dac7d21411e011f41b5f29e3608
Tiago Lopes <saxtiago14@gmail.com>
2023-01-31 22:50:18 -0300

    Feat - search value by input

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import { getRepositoriesByUser } from './service';
+import { useState } from 'react';
 
 // https://api.github.com/users/tiagolopesdev
 // https://api.github.com/users/tiagolopesdev/repos
@@ -14,29 +13,40 @@ interface RepositoryProps {
   topics: []
 }
 
+const baseUrl = 'https://api.github.com/users/';
 
 export const App = () => {
 
   const [repositories, setRepositories] = useState<RepositoryProps[]>([]);
+  const [nickname, setNickName] = useState('');
 
-  useEffect(() => {
-    //fetch('https://api.github.com/users/tiagolopesdev/repos')
-
-    fetch(getAllRepositories())
-    .then(response => response.json())
-    .then(data => setRepositories(data))
-  }, [repositories])
-
-  async function getAllRepositories() {
-    return await getRepositoriesByUser('tiagolopesdev');
+  const getAllRepositories = async (nickName: string) => {
+    if (nickName !== '') {
+      return fetch(`${baseUrl}${nickname}/repos`)
+        .then(response => response.json())
+        .then(data => {
+          setRepositories(data)
+          console.log('Repository: ', data);
+        });
+    } else if (nickName === '' && repositories !== undefined) {
+      setRepositories([]);
+    }
   }
 
   return (
     <div className="App">
+      <div>
+        <input
+          type='text'
+          placeholder='Digite o nume do usuário'
+          onChange={(e) => setNickName(e.target.value)}
+        />
+        <button onClick={() => getAllRepositories(nickname)}>SEARCH</button>
+      </div>
       <header >
         {repositories.map(repository => {
           return <>
-            <div 
+            <div
               // style={{
               //   height: 'auto',
               //   padding: '10px',
@@ -46,62 +56,63 @@ export const App = () => {
               //   color: 'white',
               //   'box-shadow': 'rgba(0, 0, 0, 0.35) 0px 5px 15px'
               // }}
+              key={repository.id}
             >
-              <h2 key={repository.id} 
-                // style={{
-                // 'display': 'flex',
-                // 'font-family': 'Montserrat, sans-serif',
-                // 'font-size': '35px'
-                // }}
+              <h2
+              // style={{
+              // 'display': 'flex',
+              // 'font-family': 'Montserrat, sans-serif',
+              // 'font-size': '35px'
+              // }}
               >{repository.name}</h2>
-              <p 
-                // style={{display: 'flex'}}
+              <p
+              // style={{display: 'flex'}}
               >{repository.description}</p>
               <div
-                // style={{
-                //   'display': 'flex',
-                //   'justify-content': 'center',
-                //   'align-items': 'center'
-                // }}
+              // style={{
+              //   'display': 'flex',
+              //   'justify-content': 'center',
+              //   'align-items': 'center'
+              // }}
               >
                 <p
-                  // style={{
-                  //   'display': 'flex',
-                  //   'font-family': 'Montserrat, sans-serif',
-                  //   'font-style': 'italic'
-                  // }}
-                >Clonar repositório</p>
-                <p 
-                  // style={{
-                  //   'display': 'flex',
-                  //   'background-color': 'rgb(32 70 157 / 39%)',
-                  //   'padding': '2px 5px',
-                  //   'border-radius': '5px'
-                  // }}
-                >{repository.clone_url}</p>
-              </div>
-              <a href={repository.svn_url} 
                 // style={{
                 //   'display': 'flex',
-                //   'justify-content': 'flex-end',
-                //   'margin-right': '20px'
+                //   'font-family': 'Montserrat, sans-serif',
+                //   'font-style': 'italic'
                 // }}
+                >Clonar repositório</p>
+                <p
+                // style={{
+                //   'display': 'flex',
+                //   'background-color': 'rgb(32 70 157 / 39%)',
+                //   'padding': '2px 5px',
+                //   'border-radius': '5px'
+                // }}
+                >{repository.clone_url}</p>
+              </div>
+              <a href={repository.svn_url}
+              // style={{
+              //   'display': 'flex',
+              //   'justify-content': 'flex-end',
+              //   'margin-right': '20px'
+              // }}
               > ir para repositorio</a>
-              <div 
+              <div
               // style={{
               //   'display': 'flex',
               //   'gap': '10px'
               // }}
               >{repository.topics.map(topic => {
-                return <p 
-                  // style={{    
-                  //   'display': 'flex',
-                  //   'box-shadow': 'rgb(0 0 0 / 16%) 0px 1px 4px',
-                  //   'padding': '5px',
-                  //   'border-radius': '20px',
-                  //   'background-color': 'rgb(255 255 255)',
-                  //   'color': 'rgb(120 120 120)'
-                  // }}
+                return <p
+                // style={{    
+                //   'display': 'flex',
+                //   'box-shadow': 'rgb(0 0 0 / 16%) 0px 1px 4px',
+                //   'padding': '5px',
+                //   'border-radius': '20px',
+                //   'background-color': 'rgb(255 255 255)',
+                //   'color': 'rgb(120 120 120)'
+                // }}
                 >{topic}</p>;
               })}</div>
             </div>
