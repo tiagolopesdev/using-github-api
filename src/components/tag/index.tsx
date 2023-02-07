66e55756a3a7adcaec13f4296465726c90ce1fc6
Tiago Lopes <saxtiago14@gmail.com>
2023-02-07 17:51:37 -0300

    Feat - tag component created with style

---
+++ src/components/tag/index.tsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+
+interface ITagProps {
+    name: string;
+}
+
+export const Tag = (props: ITagProps) => {
+    return (
+        <span className='spanStyle'>{props.name}</span>
+    );
+}
