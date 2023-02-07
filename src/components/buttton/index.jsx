0fd8be756596ff50d63ae1639d92cda55f3982fe
Tiago Lopes <saxtiago14@gmail.com>
2023-02-07 18:03:02 -0300

    Feat - button component created

---
+++ src/components/buttton/index.jsx
@@ -0,0 +1,15 @@
+
+export const Button = (props) => {
+    return (
+        <button {...props} style={{
+            'width': '10rem',
+            'padding': '0.4rem',
+            'border': 'none',
+            'borderRadius': '30px',
+            'backgroundColor': '#FFFFFF',
+            'color': '#4C86BB',
+            'fontSize': '12pt',
+            'fontWeight': 'bold'
+        }}>{props.diplayText}</button>
+    );
+}
