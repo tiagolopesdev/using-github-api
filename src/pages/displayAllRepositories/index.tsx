7baac8e903d7e1853eb06a5864615faec96232a3
Tiago Lopes <saxtiago14@gmail.com>
2023-02-15 22:37:21 -0300

    Refactor - using profile user of the context at navbar

--- src/pages/displayAllRepositories/index.tsx
+++ src/pages/displayAllRepositories/index.tsx
@@ -6,9 +6,6 @@ import { Button } from '../../components/buttton';
 import { UserProfileContext } from '../../context/user';
 import { Card } from '../../components/card';
 
-// https://api.github.com/users/tiagolopesdev
-// https://api.github.com/users/tiagolopesdev/repos
-
 interface RepositoryProps {
   id: string,
   name: string,
@@ -28,9 +25,8 @@ interface ProfileUserProps {
 export const DisplayAllRepositories = () => {
 
   const [repositories, setRepositories] = useState<RepositoryProps[]>([]);
-  const [profileUser, setProfileUser] = useState<ProfileUserProps | null>(null);
   const [nickname, setNickName] = useState('');
-  const { assigUserProfile, user, isStorage, getStorageProfileUser, getProfileUserStored } = useContext(UserProfileContext);
+  const { setProfileUserLocalStored, user, isStorage, getStorageProfileUser, getProfileUserStored } = useContext(UserProfileContext);
 
   useEffect(() => {
 
@@ -56,9 +52,9 @@ export const DisplayAllRepositories = () => {
         login: resProfile.login
       }
 
-      await assigUserProfile(profileUserProp)
+      await setProfileUserLocalStored(profileUserProp)
 
-      setProfileUser(profileUserProp);
+      await getProfileUserStored();
 
       setRepositories(await getRepositoriesByUser(nickName));
 
@@ -71,8 +67,8 @@ export const DisplayAllRepositories = () => {
 
   return (
     <div className="App">
-      <div>
-        <NavBar props={profileUser} >
+      <div>        
+        <NavBar props={user} >
           <div>
             <input
               type='text'
@@ -90,17 +86,6 @@ export const DisplayAllRepositories = () => {
               diplaytext={'Search'}
               onClick={() => getAllRepositories(nickname)}
             />
-            {/* <button
-              //onClick={() => console.log('Click in button')}
-              onClick={() => getAllRepositories(nickname)}
-            // style={{
-            //   'border': 'none',
-            //   'padding': '0.8rem',
-            //   'marginRight': '10px',
-            //   'borderRadius': '30px',
-            //   'outlineStyle': 'none'
-            // }}
-            >SEARCH</button> */}
           </div>
         </NavBar>
       </div>
@@ -118,90 +103,6 @@ export const DisplayAllRepositories = () => {
           )
         })}
       </div>
-
-
-      {/* <div
-        style={{
-          'marginTop': '10%',
-          'display': 'grid',
-          'justifyContent': 'center'
-        }}
-      >
-        {repositories.map(repository => {
-          return <>
-            <div
-              style={{
-                height: 'auto',
-                padding: '10px',
-                'borderRadius': '20px',
-                'backgroundColor': '#3f6dd5',
-                margin: '20px',
-                color: 'white',
-                'boxShadow': 'rgba(0, 0, 0, 0.35) 0px 5px 15px'
-              }}
-              key={repository.id}
-            >
-              <h2
-              // style={{
-              // 'display': 'flex',
-              // 'font-family': 'Montserrat, sans-serif',
-              // 'font-size': '35px'
-              // }}
-              >{repository.name}</h2>
-              <p
-              // style={{display: 'flex'}}
-              >{repository.description}</p>
-              <div
-              // style={{
-              //   'display': 'flex',
-              //   'justify-content': 'center',
-              //   'align-items': 'center'
-              // }}
-              >
-                <p
-                // style={{
-                //   'display': 'flex',
-                //   'font-family': 'Montserrat, sans-serif',
-                //   'font-style': 'italic'
-                // }}
-                >Clonar repositório</p>
-                <p
-                // style={{
-                //   'display': 'flex',
-                //   'background-color': 'rgb(32 70 157 / 39%)',
-                //   'padding': '2px 5px',
-                //   'border-radius': '5px'
-                // }}
-                >{repository.clone_url}</p>
-              </div>
-              <a href={repository.svn_url}
-              // style={{
-              //   'display': 'flex',
-              //   'justify-content': 'flex-end',
-              //   'margin-right': '20px'
-              // }}
-              > ir para repositorio</a>
-              <div
-              // style={{
-              //   'display': 'flex',
-              //   'gap': '10px'
-              // }}
-              >{repository.topics.map(topic => {
-                return <p
-                // style={{    
-                //   'display': 'flex',
-                //   'box-shadow': 'rgb(0 0 0 / 16%) 0px 1px 4px',
-                //   'padding': '5px',
-                //   'border-radius': '20px',
-                //   'background-color': 'rgb(255 255 255)',
-                //   'color': 'rgb(120 120 120)'
-                // }}
-                >{topic}</p>;
-              })}</div>
-            </div>
-          </>
-        })}
-      </div> */}
     </div >
   );
 }
