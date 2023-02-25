046e2b9ef2946cb56b9a686dedb16985e5e89bc9
Victor Silva <victor.silvaPC@hotmail.com>
2023-02-25 18:23:04 -0300

    :tada: adding improvements

--- src/@types/repositories.ts
+++ src/@types/repositories.ts
@@ -1,4 +1,3 @@
-
 export interface IRepositoryProps {
     id: string,
     name: string,
--- src/components/actionIcons/style.ts
+++ src/components/actionIcons/style.ts
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const ImgStyle = styled.img`
   padding: 15%;
-  width: '40px';
-  margin: '15px';
+  width: '2.5rem';
+  margin: '.9375rem';
 `;
