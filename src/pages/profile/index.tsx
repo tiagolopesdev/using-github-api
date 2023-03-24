d3d4ebf564a9bc1495febf9e94093e15f4711ca0
Tiago Lopes <saxtiago14@gmail.com>
2023-03-23 22:07:01 -0300

    Feat: start implementation page show profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,6 +1,8 @@
+import { Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 import { useContext } from "react";
 import { NavBar } from "../../components/Navbar";
 import { UserProfileContext } from "../../context/user";
+import { Button } from "../../components/Button";
 
 export const Profile = () => {
 
@@ -19,12 +21,69 @@ export const Profile = () => {
     return (
         <>
             <NavBar />
-            <h1>Acesso aos dados do perfil</h1>
+            <div
+                style={{ marginTop: '200px' }}
+            >
+                <Card sx={{ maxWidth: '80%' }}>
+                    {/* <img src={user?.avatar_url} /> */}
+                    {/* <CardMedia
+                        component="img"
+                        alt="green iguana"
+                        height="140"
+                        image={`${user?.avatar_url}`}
+                    /> */}
+                    <CardContent>
+                        <div style={{ display: "flex" }} >
+                            <div>
+                                <img src={user?.avatar_url} style={{ borderRadius: '50%' }} />
+                                <div style={{ display: 'grid', justifyItems: "center" }} >
+                                    <p>Biografia</p>
+                                    <p>Localização</p>
+                                </div>
+                            </div>
+                            <div >
+                                <h1>NICKNAME</h1>
+                                <h4>EMAIL</h4>
+                                <div style={{ display: "flex", justifyContent: "end" }} >
+                                    <div style={{
+                                        display: 'grid',
+                                        justifyItems: 'center'
+                                    }}>
+                                        <p style={{ marginBottom: '0px' }}>000</p>
+                                        <p>Seguidores</p>
+                                    </div>
+                                    <div style={{
+                                        marginLeft: '20px',
+                                        display: 'grid',
+                                        justifyItems: 'center'
+                                    }} >
+                                        <p style={{ marginBottom: '0px' }}>000</p>
+                                        <p>Seguindo</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {user?.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button>Share</Button>
+                        <Button>Learn More</Button>
+                    </CardActions>
+                </Card>
+            </div>
+
+            {/* <h1>Acesso aos dados do perfil</h1>
             <div>
                 <h1>{user?.name}</h1>
                 <h1>{user?.public_repos}</h1>
                 <h1>{user?.avatar_url}</h1>
-            </div>
+            </div> */}
         </>
     );
 }
