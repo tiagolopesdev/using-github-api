7f38507d9f34475da7b3923d2d4daac4fdf631b1
Tiago Lopes <saxtiago14@gmail.com>
2023-04-16 11:58:00 -0300

    Feat: new version modal with information of the github profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,55 +1,50 @@
-import { Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
 import { useContext } from "react";
 import { NavBar } from "../../components/Navbar";
 import { UserProfileContext } from "../../context/user";
-import { Button } from "../../components/Button";
 
 export const Profile = () => {
 
     const { user } = useContext(UserProfileContext);
 
-    // useEffect(() => {
-
-    //     getStorageProfileUser()
-
-    //     if (isStorage) {
-    //         getProfileUserStored()
-    //     }
-
-    // }, [isStorage])
-
     return (
         <>
             <NavBar />
             <div
-                style={{ marginTop: '200px' }}
+                style={{
+                    marginTop: '200px',
+                    display: 'flex',
+                    justifyContent: 'center'
+                }}
             >
-                <Card sx={{ maxWidth: '80%' }}>
-                    {/* <img src={user?.avatar_url} /> */}
-                    {/* <CardMedia
-                        component="img"
-                        alt="green iguana"
-                        height="140"
-                        image={`${user?.avatar_url}`}
-                    /> */}
+                <Card sx={{ maxWidth: '100%', padding: '2rem' }}>
                     <CardContent>
                         <div style={{ display: "flex" }} >
                             <div>
-                                <img src={user?.avatar_url} style={{ borderRadius: '50%' }} />
+                                <img src='https://avatars.githubusercontent.com/u/58925056?v=4' style={{ borderRadius: '50%', width: '15rem' }} />
                                 <div style={{ display: 'grid', justifyItems: "center" }} >
-                                    <p>Biografia</p>
+                                    <p style={{
+                                        fontSize: '18pt',
+                                        fontWeight: 'bold'
+                                    }}>Biografia</p>
                                     <p>Localização</p>
                                 </div>
                             </div>
-                            <div >
-                                <h1>NICKNAME</h1>
-                                <h4>EMAIL</h4>
-                                <div style={{ display: "flex", justifyContent: "end" }} >
+                            <div
+                                style={{
+                                    marginLeft: '10%',
+                                    marginTop: '5%',
+                                    minWidth: '50%'
+                                }}
+                            >
+                                <h1>Tiago Lopes</h1>
+                                <p>saxtiago14@gmail.com</p>
+                                <div style={{ display: "flex", justifyContent: "end", marginTop: '15%' }} >
                                     <div style={{
                                         display: 'grid',
                                         justifyItems: 'center'
                                     }}>
-                                        <p style={{ marginBottom: '0px' }}>000</p>
+                                        <h4 style={{ marginBottom: '0px' }}>000</h4>
                                         <p>Seguidores</p>
                                     </div>
                                     <div style={{
@@ -57,33 +52,36 @@ export const Profile = () => {
                                         display: 'grid',
                                         justifyItems: 'center'
                                     }} >
-                                        <p style={{ marginBottom: '0px' }}>000</p>
+                                        <h4 style={{ marginBottom: '0px' }}>000</h4>
                                         <p>Seguindo</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <Typography gutterBottom variant="h5" component="div">
-                            {user?.name}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
-                        </Typography>
                     </CardContent>
-                    <CardActions>
-                        <Button>Share</Button>
-                        <Button>Learn More</Button>
+                    <CardActions
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Button
+                            variant="contained"
+                            style={{                                
+                                borderRadius: '20px',
+                                padding: '5px 50px'
+                            }}
+                        >Ver repositórios</Button>
+                        <Button
+                            variant="outlined"
+                            style={{                                
+                                borderRadius: '20px',
+                                padding: '5px 50px'
+                            }}
+                        >Perfil do github</Button>
                     </CardActions>
                 </Card>
             </div>
-
-            {/* <h1>Acesso aos dados do perfil</h1>
-            <div>
-                <h1>{user?.name}</h1>
-                <h1>{user?.public_repos}</h1>
-                <h1>{user?.avatar_url}</h1>
-            </div> */}
         </>
     );
 }
